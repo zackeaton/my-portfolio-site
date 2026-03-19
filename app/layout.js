5d353085648a920985d9caca7fd2af13bab09efb
Zack <zackaryeaton@gmail.com>
2026-03-19 14:51:19 -0400

    Trying Vercel Analytics again

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +23,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}<Analytics /></body>
     </html>
   );
 }
